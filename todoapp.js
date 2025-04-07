7f48cbf777fe88c29a99a52bcfe33a26578d3015
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-07 15:21:29 +0530

    update file

--- todoapp.js
+++ todoapp.js
@@ -4,7 +4,21 @@ let inp=document.querySelector("input");
 btn.addEventListener("click",function(){
     let item=document.createElement("li");
     item.innerText=inp.value;
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
     ul.appendChild(item);
     console.log(inp.value);
     inp.value="";
 });
+
+//let delBtns=document.querySelectorAll(".delete");
+//for(delBtn of delBtns){
+   // delBtn.addEventListener("click",function(){
+     //   let par =this.parentElement;
+       // console.log(par);
+        //par.remove();
+   // });
+//}
